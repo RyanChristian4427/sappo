385cdfeeb71745ffce61514abf53823f06cb9d87
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-02 13:47:02 +0000

    style: Updated styling on login page

--- react-client/src/components/Login/index.tsx
+++ react-client/src/components/Login/index.tsx
@@ -1,58 +1,64 @@
-import React from "react";
-import {inject, observer} from "mobx-react";
-import {AuthStore} from "../../stores/modules/authStore";
+import React from 'react';
+import {inject, observer} from 'mobx-react';
+import {AuthStore} from '../../stores/modules/authStore';
 import {Redirect} from 'react-router-dom';
 
-import "./Login.scss"
+import './Login.scss'
 
 interface InjectedProps {
-    authStore: AuthStore
+    authStore: AuthStore;
 }
 
 @inject('authStore')
 @observer
 export default class Login extends React.Component<{}, {}> {
 
-    public state: any = {
+    public state = {
         redirect: false
     };
 
-    public get injectedProps() {
+    public get injectedProps(): InjectedProps {
         return this.props as InjectedProps;
     }
 
-    public username: string = '';
+    public username = '';
 
-    render() {
+    render(): React.ReactNode {
         if (this.state.redirect) {
             return <Redirect to='/' />;
         }
 
         return (
-            <div className="Login">
-                <header className="Header">
-                    <h1 className="is-size-1">
-                        Login Page
-                    </h1>
-                </header>
-                <div className="login-form">
-                    <div className="field">
-                        <label className="label">User Name</label>
-                        <div className="control">
-                            <input className="input" type="text" placeholder="User Name" onChange={this.handleChange()}/>
+            <div>
+                <section className="hero is-xanadu-light">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Login
+                            </h1>
                         </div>
                     </div>
-                    <button className="button is-primary is-right" onClick={this.handleSubmitForm}>Submit</button>
+                </section>
+                <div className="card">
+                    <div className="container" id="layered-background">
+                        <div className="field">
+                            <label className="label">User Name</label>
+                            <div className="control">
+                                <input className="input" type="text" placeholder="User Name" onChange={this.handleChange()}/>
+                            </div>
+                        </div>
+                        <button className="button is-xanadu-dark is-right" onClick={this.handleSubmitForm}>Submit</button>
+                    </div>
                 </div>
             </div>
         );
     }
 
-    private handleChange = () => (event: React.ChangeEvent<HTMLInputElement>) => {
+    private handleChange = () => (event: React.ChangeEvent<HTMLInputElement>): void => {
         this.username =  event.target.value;
     };
 
-    private handleSubmitForm = (e: React.FormEvent<HTMLButtonElement>) => {
+    private handleSubmitForm = (e: React.FormEvent<HTMLButtonElement>): void => {
         e.preventDefault();
         const { username } = this;
         this.injectedProps.authStore
