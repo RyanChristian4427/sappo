d31fdc41a31e473b547d247768c7026a0f6804de
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-03 23:53:37 +0000

    chore(lint): Linting of express server

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -1,26 +1,29 @@
-import express from "express";
-import bodyParser from "body-parser";
-import path from "path";
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
 
 // Controllers (route handlers)
-import * as authController from "./controllers/auth";
+import * as authController from './controllers/auth';
 
 // Create Express server
 const app = express();
 
 // Express configuration
-app.set("port", process.env.PORT || 8000);
+app.set('port', process.env.PORT || 8000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+    express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 })
 );
 
+const x = 5;
 /**
  * API examples routes.
  */
-app.post("/api/v1/users/login", authController.login);
+if (5 === x) {
+    app.post('/api/v1/users/login', authController.login);
+}
 
 
 export default app;
--- express-server/src/server.ts
+++ express-server/src/server.ts
@@ -1,6 +1,7 @@
-import errorHandler from "errorhandler";
+import errorHandler from 'errorhandler';
+import socketIo from 'socket.io';
 
-import app from "./app";
+import app from './app';
 
 /**
  * Error Handler. Provides full stack - remove for production
@@ -10,13 +11,29 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
+const server = app.listen(app.get('port'), () => {
     console.log(
-        "  App is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
+        '  App is running at http://localhost:%d in %s mode',
+        app.get('port'),
+        app.get('env')
     );
-    console.log("  Press CTRL-C to stop\n");
+    console.log('  Press CTRL-C to stop\n');
+});
+
+// interface SocketConnection {
+//     username: string;
+// }
+
+const io = socketIo(server);
+io.on('connection', (socket: any) => {
+    console.log('New User connected!');
+    socket.username = 'Anonymous';
+    console.log(socket.username);
+
+    socket.on('change_username', (newName: string) => {
+        socket.username = newName;
+        console.log(socket.username);
+    });
 });
 
 export default server;
