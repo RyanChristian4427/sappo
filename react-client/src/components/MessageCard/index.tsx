f0fae54fae7d1767cfc769dcf42f61328d0db42a
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-07 15:29:06 +0000

    feat: Added additional details to be conditionally rendered to the message

--- react-client/src/components/MessageCard/index.tsx
+++ react-client/src/components/MessageCard/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import {ReturnedChatMessage} from 'src/models/ChatMessage';
+import {displayTimeStamp} from './utility';
+
 import './MessageCard.scss';
-import {ReturnedChatMessage} from '../../models/ChatMessage';
+
 
 interface MessageProps {
     message: ReturnedChatMessage;
@@ -9,56 +12,53 @@ interface MessageProps {
 }
 
 export default class MessageCard extends React.Component<MessageProps, {}> {
-
-    // Taken from another project of mine, a webmail client, where I put quite a bit
-    // of thought into how the date should be displayed. Made sense to reuse. Probably overkill for now, but scalable.
-    // Link: https://github.com/RyanChristian4427/tempest/blob/master/vue-client/src/components/tempest/EmailCard.vue
-    private displayTimeStamp(datetime: string): string {
-        const emailDate = new Date(datetime);
-
-        const today = new Date();
-
-        const dayInMillis = 60 * 60 * 24 * 1000;
-        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-        // If an email is more than 6 months old (irrespective of date within month), and is the year previous,
-        // give it the full date format. If it was in the past 24 hours, give it a timestamp, otherwise,
-        // give it mmm / dd format
-        const sixMonthsAgo = new Date();
-        sixMonthsAgo.setMonth(today.getMonth() - 6, 0);
-        return sixMonthsAgo > emailDate && today.getFullYear() !== emailDate.getFullYear()
-            ? months[emailDate.getMonth()] + ' ' + emailDate.getUTCDate() + ' ' + emailDate.getFullYear()
-            : today.getTime() - emailDate.getTime() < dayInMillis
-                ? this.twentyFourHourToTwelve(emailDate.getHours()) + ':' + (emailDate.getMinutes()).toString().padStart(2, '0')
-                : months[emailDate.getMonth()] + ' ' + emailDate.getUTCDate();
-    }
-
-    // Quickly grabbed from Stack Overflow
-    // Link: https://stackoverflow.com/questions/10556879/changing-the-1-24-hour-to-1-12-hour-for-the-gethours-method
-    private twentyFourHourToTwelve(currentHours: number): number {
-        return (currentHours + 24) % 12 || 12;
-    }
-
     render(): React.ReactNode {
         const message = this.props.message;
 
-        const messageContent = (): React.ReactNode => {
+        const messageRow = (label: string, data: string): React.ReactNode => {
             return (
-                <div className="container level-item">
-                    <h6>{this.displayTimeStamp(message.datetimestamp)}</h6>
-                    <div className="level">
-                        <div className="level-right">
-                            <h1 className="level-item is-size-5">{message.username}: </h1>
-                        </div>
-                        <h2 className="level-item is-size-5">{message.text}</h2>
+                <div className="level">
+                    <div className="level-left">
+                        <h1 className="level-item is-size-5">{label}: </h1>
+                    </div>
+                    <div className="level-right">
+                        <h2 className="level-item is-size-5">{data}</h2>
                     </div>
                 </div>
             );
         };
 
+        const abundance = (message.abundance != 0)
+            ? messageRow('Abundance', message.abundance.toString())
+            : null;
+
+        const coordinates = (!message.coordinates.every((e) => [0,0].includes(e)))
+            ? messageRow('Coordinates', message.coordinates.toString())
+            : null;
+
+        const species = (message.species != '')
+            ? messageRow('Species', message.species)
+            : null;
+
+        const temperature = (message.temperature != 0)
+            ? messageRow('Current Temperature', message.temperature.toString() + '° Celsius')
+            : null;
+
+        const messageContent = (): React.ReactNode => {
+            return (
+                <div className="container level-item">
+                    <h6>{displayTimeStamp(message.datetimestamp)}</h6>
+                    {messageRow(message.username, message.text)}
+                    {abundance}
+                    {coordinates}
+                    {species}
+                    {temperature}
+                </div>
+            );
+        };
+
+        // Decides what side of the screen a message should appear on. If you sent it, right side, someone else, left.
         const alignedContent = (message.username !== this.props.currentUser)
-            // TODO It might be my inexperience with the framework, but it seems that without the opposite level item, there is no effect,
-            // i.e., all items end up on the left. To fix this, I've just inserted empty elements to make the spacing work. Come back to this later?
             ? <React.Fragment>
                 <div className="level-left">{messageContent()}</div>
                 <div className="level-right"/>
---
+++ react-client/src/components/MessageCard/utility.ts
@@ -0,0 +1,28 @@
+// Quickly grabbed from Stack Overflow
+// Link: https://stackoverflow.com/questions/10556879/changing-the-1-24-hour-to-1-12-hour-for-the-gethours-method
+export function twentyFourHourToTwelve(currentHours: number): number {
+    return (currentHours + 24) % 12 || 12;
+}
+
+// Taken from another project of mine, a webmail client, where I put quite a bit
+// of thought into how the date should be displayed. Made sense to reuse. Probably overkill for now, but scalable.
+// Link: https://github.com/RyanChristian4427/tempest/blob/master/vue-client/src/components/tempest/EmailCard.vue
+export function displayTimeStamp(datetime: string): string {
+    const emailDate = new Date(datetime);
+
+    const today = new Date();
+
+    const dayInMillis = 60 * 60 * 24 * 1000;
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    // If an email is more than 6 months old (irrespective of date within month), and is the year previous,
+    // give it the full date format. If it was in the past 24 hours, give it a timestamp, otherwise,
+    // give it mmm / dd format
+    const sixMonthsAgo = new Date();
+    sixMonthsAgo.setMonth(today.getMonth() - 6, 0);
+    return sixMonthsAgo > emailDate && today.getFullYear() !== emailDate.getFullYear()
+        ? months[emailDate.getMonth()] + ' ' + emailDate.getUTCDate() + ' ' + emailDate.getFullYear()
+        : today.getTime() - emailDate.getTime() < dayInMillis
+            ? twentyFourHourToTwelve(emailDate.getHours()) + ':' + (emailDate.getMinutes()).toString().padStart(2, '0')
+            : months[emailDate.getMonth()] + ' ' + emailDate.getUTCDate();
+}
