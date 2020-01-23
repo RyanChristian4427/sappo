17a10c828f0a734b2bdd5091e47352a811c58054
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-23 14:48:58 +0000

    feat(dependency): Added prettier to the app

--- react-client/.eslintrc.js
+++ react-client/.eslintrc.js
@@ -1,8 +1,10 @@
 module.exports =  {
     parser:  '@typescript-eslint/parser',
     extends:  [
+        'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended'
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
     ],
     settings: {
         react: {
@@ -14,13 +16,17 @@ module.exports =  {
         sourceType:  'module',
     },
     rules:  {
-        "quotes": "off",
-        "@typescript-eslint/quotes": ["warn", "single"],
-        "@typescript-eslint/no-use-before-define": ["error", { "classes": false }],
-        "arrow-parens": "warn",
-        "@typescript-eslint/ban-ts-ignore": ["off"],
-        "@typescript-eslint/interface-name-prefix": "off",
-        "semi": "off",
-        "@typescript-eslint/semi": ["warn"]
+        'indent': ['warn', 4, { "SwitchCase": 1 }],
+        'react/jsx-indent': ['warn', 4],
+        'react/jsx-indent-props': ['warn', 4],
+        'react/no-unknown-property': ['error', { ignore: ['class'] }],
+        'quotes': 'off',
+        '@typescript-eslint/quotes': ['warn', 'single'],
+        '@typescript-eslint/no-use-before-define': ['warn'],
+        'arrow-parens': 'warn',
+        '@typescript-eslint/ban-ts-ignore': ['warn'],
+        '@typescript-eslint/interface-name-prefix': 'off',
+        'semi': 'off',
+        '@typescript-eslint/semi': ['warn'],
     },
 };
